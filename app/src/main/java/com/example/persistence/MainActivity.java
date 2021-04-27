95496c96e7e5b40db7546968c5b12110ebd18aa3
c20emili <c20emili@student.his.se>
2021-04-27 19:50:37 +0200

    added an on click for the write button that changes the values of the edittexts to fit the addLake method? and uses the method? to add lake data.

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase database;
-    private Helper databaseHelper;
+    private Helper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create
-        databaseHelper = new Helper(this);
-        database = databaseHelper.getWritableDatabase();
+        helper = new Helper(this);
+        database = helper.getWritableDatabase();
+        Button write = findViewById(R.id.write);
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText1 =findViewById(R.id.etext1);
+                EditText editText2 =findViewById(R.id.etext2);
+                EditText editText3 =findViewById(R.id.etext3);
+                String n= editText1.getText().toString();
+                int s=Integer.parseInt(editText2.getText().toString());// konverterar string värdet till int
+                int d=Integer.parseInt(editText3.getText().toString());
+                addLake(n,s,d);
+            }
+        });
+
 
 
     }
 
-    private long addLake(String name, int size) {
+    private long addLake(String name, int size, int depth) {
         ContentValues values = new ContentValues();
         values.put(Tables.Lake.COLUMN_NAME_NAME, name);
         values.put(Tables.Lake.COLUMN_NAME_SIZE, size);
+        values.put(Tables.Lake.COLUMN_NAME_DEPTH, depth);
         return database.insert(Tables.Lake.TABLE_NAME, null, values);
     }
 }
