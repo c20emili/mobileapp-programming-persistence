2b78640ca6728396dccb7bf29d73c7f7eb037a82
c20emili <c20emili@student.his.se>
2021-04-27 19:18:18 +0200

    created an instance of the database and helper in main and added a function to add name and size.

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase database;
+    private Helper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create
+        databaseHelper = new Helper(this);
+        database = databaseHelper.getWritableDatabase();
+
+
+    }
+
+    private long addLake(String name, int size) {
+        ContentValues values = new ContentValues();
+        values.put(Tables.Lake.COLUMN_NAME_NAME, name);
+        values.put(Tables.Lake.COLUMN_NAME_SIZE, size);
+        return database.insert(Tables.Lake.TABLE_NAME, null, values);
     }
 }
