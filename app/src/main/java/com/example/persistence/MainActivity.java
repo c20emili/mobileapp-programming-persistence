7ad133028a04665ca99883f169605e8e914cadc3
c20emili <c20emili@student.his.se>
2021-05-04 15:20:27 +0200

    added a function to fetch from the database

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,26 +3,32 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase database;
     private Helper helper;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create
         helper = new Helper(this);
         database = helper.getWritableDatabase();
+
         Button write = findViewById(R.id.write);
         write.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
     }
 
     private long addLake(String name, int size, int depth) {
@@ -47,5 +52,20 @@ public class MainActivity extends AppCompatActivity {
         values.put(Tables.Lake.COLUMN_NAME_SIZE, size);
         values.put(Tables.Lake.COLUMN_NAME_DEPTH, depth);
         return database.insert(Tables.Lake.TABLE_NAME, null, values);
+    }
+    private List<Lake> fetchLakes() {
+        Cursor cursor = database.query(Tables.Lake.TABLE_NAME, null, null, null, null, null, null);
+        List<Lake> lakes = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            Lake lake = new Lake(
+                    cursor.getLong(cursor.getColumnIndexOrThrow(Tables.Lake.COLUMN_NAME_ID)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(Tables.Lake.COLUMN_NAME_NAME)),
+                    cursor.getInt(cursor.getColumnIndexOrThrow(Tables.Lake.COLUMN_NAME_SIZE)),
+                    cursor.getInt(cursor.getColumnIndexOrThrow(Tables.Lake.COLUMN_NAME_DEPTH))
+            );
+            lakes.add(lake);
+        }
+        cursor.close();
+        return lakes;
     }
 }
